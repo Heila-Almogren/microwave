ccea75d7163be4e60d5bebc92d15a721451ca3f1
Heila Al-Mogren <55871567+Heila-Almogren@users.noreply.github.com>
2022-01-28 06:47:00 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'microwave';
+  constructor(private http: HttpClient) {
+
+  }
 }
